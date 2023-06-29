2b2befad55845492cbfcccba3111de73b25e5cf9
Platon Kobylyatskiy <platonkoby@MacBook-Pro.local>
2023-06-29 12:08:07 +0100

    language change visuals

--- src/components/NavBar/LangDropDown.js
+++ src/components/NavBar/LangDropDown.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Dropdown } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { BsGlobe2 } from 'react-icons/bs'
 import { savePreferedLanguage } from '../../extras/localStorageFuncs'
 
 function LangDropDown() {
+
+    const [engToggle, setEngToggle] = useState()
 
     const [t, i18n] = useTranslation('common')
 
@@ -18,23 +20,21 @@ function LangDropDown() {
         savePreferedLanguage('pt')
     }
     
+    useEffect(() => {
+        if (i18n.language == 'en') {
+            setEngToggle(true)
+        } else {
+            setEngToggle(false)
+        }
+
+    }, [i18n.language])
 
   return (
-    <Dropdown drop='none'>
-        <Dropdown.Toggle 
-        style={{
-        all: 'unset',
-        
-    }}
-    >
-            <BsGlobe2 size={23} />
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-            <Dropdown.Item onClick={switchToEnglish}>English</Dropdown.Item>
-            <Dropdown.Item onClick={switchToPortuguese}>Portuguese</Dropdown.Item>
-        </Dropdown.Menu>
-    </Dropdown>
+   <div className='nav-lang'>
+    <div onClick={switchToEnglish} className={`lang-el ${engToggle && 'lang-chosen'}`}>EN</div>
+    <div className="vertical-line"></div>
+    <div onClick={switchToPortuguese} className={`lang-el ${!engToggle && 'lang-chosen'}`}>PT</div>
+   </div>
   )
 }
 
