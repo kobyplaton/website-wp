4c1adda0e70773c33241d4d1f4854db0fae1f6ce
Platon Kobylyatskiy <platonkoby@MacBook-Pro.local>
2023-07-24 15:20:25 +0100

    changes

--- src/sections/MiniGallery/MiniGallery.js
+++ src/sections/MiniGallery/MiniGallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/MiniGallery.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { getImageList } from '../../firebase/storage';
@@ -7,6 +7,7 @@ import { useWindowSize } from '@uidotdev/usehooks'
 import MiniGalleryImg from './MiniGalleryImg';
 import Lightbox from 'yet-another-react-lightbox'
 import CustomSlide from '../../pages/Gallery/CustomSlide';
+import { getCollectionItems } from '../../firebase/firestore';
 
 const useImageSize = () => {
     const {width} = useWindowSize()
@@ -23,13 +24,39 @@ function MiniGallery() {
 
     const [lightbox, toggleLightbox] = React.useState()
     const [imageList, setImageList] = React.useState([])
+    const [imageDetails, setImageDetails] = React.useState([])
+    const [slides, setSlides] = React.useState([])
     const optimalLength = useImageSize()
+    const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0)
+
+    const handleImageClick = (index) => {
+      setCurrentSlideIndex(index)
+      toggleLightbox(true)
+    }
 
     React.useEffect(() => {
       getImageList(setImageList)
-  
   }, [])
   
+  React.useEffect(() => {
+    const files = getCollectionItems()
+    files.then((res) => res.forEach((doc) => setImageDetails((imageDetails) => [...imageDetails, doc.data()])))
+  }, [])
+
+  React.useEffect(() => {
+
+    let slidesArr = [];
+
+    for (let image of imageList) {
+      for (let detail of imageDetails) {
+        if (image.src.includes(detail.id)) {
+          slidesArr.push({src: image.src, ...detail})
+        }
+      }
+    }
+    setSlides(slidesArr)
+  }, [imageList, imageDetails])
+
 
   return (
     <>
@@ -39,15 +66,21 @@ function MiniGallery() {
       }}
         slides={imageList}
         open={lightbox}
+        index={currentSlideIndex}
         close={() => toggleLightbox(false)}
          />
-        {imageList.map((image) => (
+        {slides.map((slide, index) => (
+          <div className="gallery-item-container">
+          <div className='image-details'>
+            {slide.details}
+          </div>
           <div className="img-box">
-            <img onClick={() => toggleLightbox(true)} key={nanoid()} src={image.src} width={250} height={350}></img>
+            <img onClick={() => handleImageClick(index)} key={nanoid()} src={slide.src} width={250} height={350}></img>
+          </div>
           </div>
         ))}
       </div>
-      Come to our store to see over 40 more wall prints!
+      Come to our showroom to see over 40 wall prints!
     </>
   )
 }
